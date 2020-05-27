c4128b8aec4c9f367e7d2a4f5c6cf4d149d3f81f
Michail Roesli <misha.roesli@gmail.com>
2020-05-27 15:52:51 -0700

    add base source files

---
+++ src/config.h
@@ -0,0 +1,18 @@
+/*
+ *
+ * config.h
+ *
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+#ifndef VERBOSE
+#define VERBOSE 0
+#endif
+
+/* Includes */
+
+/* Main Definitions */
+
+#endif /* CONFIG_H_ */
---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ *
+ * main.c
+ *
+ */
+
+#include <stdio.h>
+volatile int c;
+
+int add(int a, int b) { return a + b; }
+
+int main(void)
+{
+    int a = 1, b = 2;
+    c = add(a, b);
+    printf("a + b = %i\n", c);
+    exit(1);
+}

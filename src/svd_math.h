7d12ce703a479155c2a76f7215e3144da1d4aa29
rtulip <robert.tulip97@gmail.com>
2020-08-06 10:37:59 -0700

    Fix: Got sweep() working using the new fixed point functions

--- src/svd_math.h
+++ src/svd_math.h
@@ -27,6 +27,7 @@ typedef fixed_point_double_t fixed_point_m_tmp_dp_t;
 #define SCALE_FACTOR_M_DP SCALE_FACTOR_M_tmp + SCALE_FACTOR_V + 1
 
 fixed_point_t convert_to_fixed(floating_point_t f, size_t scale_factor);
+fixed_point_double_t fixed_point_mul(fixed_point_t, fixed_point_t);
 fixed_point_u_dp_t fixed_point_mul_u_x_u(fixed_point_u_t, fixed_point_u_t);
 fixed_point_m_tmp_dp_t fixed_point_mul_u_x_m(fixed_point_u_t, fixed_point_m_t);
 fixed_point_m_tmp_t truncate_m_tmp(fixed_point_m_tmp_dp_t);
@@ -34,5 +35,9 @@ fixed_point_m_dp_t fixed_point_mul_m_x_v(fixed_point_m_tmp_t, fixed_point_v_t);
 fixed_point_v_dp_t fixed_point_mul_v_x_v(fixed_point_v_t, fixed_point_v_t);
 
 floating_point_t convert_to_floating(fixed_point_double_t f, size_t scale_factor);
+
+static const fixed_point_u_t one_u = (1 << SCALE_FACTOR_U);
+static const fixed_point_m_t one_m = (1 << SCALE_FACTOR_M);
+static const fixed_point_v_t one_v = (1 << SCALE_FACTOR_V);
 
 #endif

c18e113996b2ece38209abf15d86c699324a728e
rtulip <robert.tulip97@gmail.com>
2020-08-07 13:23:14 -0700

    Feat: going back a step

--- src/test_neon.c
+++ src/test_neon.c
@@ -20,33 +20,41 @@ void print_matrix(const fixed_point_t *m)
         }
         printf(" %d ]\n", *(m + i * N + M - 1));
     }
+    printf("\n");
 }
+
+A = {
+    {0x1, 0x2, 0x3, 0x4},
+    {0x5, 0x6, 0x7, 0x8},
+    {0x9, 0xA, 0xB, 0xC},
+    {0xD, 0xE, 0xF, 0x0},
+};
+
+B = {
+    {0x1, 0x2, 0x3, 0x4},
+    {0x5, 0x6, 0x7, 0x8},
+    {0x9, 0xA, 0xB, 0xC},
+    {0xD, 0xE, 0xF, 0x0},
+};
 
 int main(void)
 {
     int i, j;
     int32x2_t A_neon, B_neon, SUM_neon;
 
-    for (i = 0; i < N; i++)
-        for (j = 0; j < M; j++)
-        {
-            A[i][j] = 7 * i;
-            B[i][j] = 15 * j;
-        }
-
     print_matrix((const fixed_point_t *)&A[0][0]);
     print_matrix((const fixed_point_t *)&B[0][0]);
 
-    for (i = 0; i < N; i += 4)
-        for (j = 0; j < M; j++)
-        {
-            A_neon = vld1_s32((const fixed_point_t *)&A[i][j]);
-            B_neon = vld1_s32((const fixed_point_t *)&B[i][j]);
-            SUM_neon = vadd_s32(A_neon, B_neon);
-            vst1_s32((fixed_point_t *)&SUM[i][j], SUM_neon);
-        }
+    // for (i = 0; i < N; i += 4)
+    //     for (j = 0; j < M; j++)
+    //     {
+    //         A_neon = vld1_s32((const fixed_point_t *)&A[i][j]);
+    //         B_neon = vld1_s32((const fixed_point_t *)&B[i][j]);
+    //         SUM_neon = vadd_s32(A_neon, B_neon);
+    //         vst1_s32((fixed_point_t *)&SUM[i][j], SUM_neon);
+    //     }
 
-    print_matrix((const fixed_point_t *)&SUM[0][0]);
+    // print_matrix((const fixed_point_t *)&SUM[0][0]);
 
     exit(0);
 }
